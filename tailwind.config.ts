414f2d851e0f74f87af1627b076e17306a75eede
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-08 22:23:15 +0100

    [EEMW-2] | setup variant colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,47 +23,34 @@ const config: Config = {
         "eros-red": "var(--eros-red)",
         "eros-yellow": "var(--eros-yellow)",
         "eros-bg": "var(--eros-bg)",
-        // TODO: SHADNC Colors, lets update them soon
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+          DEFAULT: "var(--eros-red)",
+          foreground: "var(--foreground)",
         },
         popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+          DEFAULT: "var(--eros-black)",
+          foreground: "var(--foreground)",
         },
         primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
+          DEFAULT: "var(--eros-pink)",
+          dark: "var(--)",
+          foreground: "var(--foreground)",
         },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
+        success: {
+          DEFAULT: "var(--eros-green)",
+          foreground: "var(--foreground)",
         },
         destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+          DEFAULT: "var(--eros-red)",
+          foreground: "var(--foreground)",
         },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        chart: {
-          "1": "hsl(var(--chart-1))",
-          "2": "hsl(var(--chart-2))",
-          "3": "hsl(var(--chart-3))",
-          "4": "hsl(var(--chart-4))",
-          "5": "hsl(var(--chart-5))",
+        outline: {
+          DEFAULT: "var(--eros-white)",
+          foreground: "var(--eros-pink)",
         },
+        border: "var(--foreground)",
       },
       fontSize: {
         "4xl": ["2.986rem", "3.75rem"],
