15a27bbbe8430f7cf681f6e7251117ee6d1c4317
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-08 22:33:28 +0100

    [EEMW-2] | adjust button variants and sizes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,27 +9,25 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        primary:
+          "bg-primary text-primary-foreground hover:opacity-85",
+        success:
+          "bg-success text-success-foreground hover:opacity-85",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:opacity-85",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+          "border border-primary bg-background hover:bg-outline text-outline-foreground hover:text-outline-foreground",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        sm: "h-8 rounded-md px-4 text-xs",
+        md: "h-9 px-6 py-2",
+        lg: "h-10 rounded-md px-9",
+        icon: "h-9 w-9", // icon button, equivalent size to default "md" but squared.
       },
     },
     defaultVariants: {
-      variant: "default",
-      size: "default",
+      variant: "primary",
+      size: "md",
     },
   }
 );
@@ -43,6 +41,7 @@ export interface ButtonProps
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
