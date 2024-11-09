c02e6f3413c3593670d5674923c8a4f6a404d57e
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-09 22:51:28 +0100

    [EEMW-10] | add navbar component with a sidebar on mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,110 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { GlobeIcon, MenuIcon, XIcon } from "lucide-react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import { navigationLinks } from "./constants";
+
+const containerClass = "mx-auto my-0 lg:max-w-[1440px]";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="bg-background">
+      <nav
+        className={cn(
+          "relative px-4 py-6 flex justify-between items-center",
+          containerClass
+        )}
+      >
+        <Link href="/">
+          <Image
+            alt="Eros Elite logo"
+            className="dark:invert"
+            height={54}
+            priority
+            sizes="(max-width: 1023px) 100px, (min-width: 1023px) 140px"
+            src="/EE_LOGO_SMALL.webp"
+            width={140}
+          />
+        </Link>
+        <div className="lg:hidden">
+          <button
+            className="navbar-burger flex items-center text-foreground p-3 hover:text-primary"
+            onClick={() => setIsOpen(!isOpen)}
+            type="button"
+          >
+            <MenuIcon />
+          </button>
+        </div>
+        <ul
+          className={`${
+            isOpen ? "block" : "hidden"
+          } absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-8 max-lg:hidden`}
+        >
+          {navigationLinks.map(({ label, href }) => (
+            <li className="min-w-fit" key={label}>
+              <Link
+                className="text-base text-foreground hover:text-primary"
+                href={href}
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="cursor-pointer hidden lg:flex lg:ml-auto hover:text-primary">
+          <GlobeIcon />
+        </div>
+      </nav>
+
+      {/* Mobile */}
+      <div
+        className={`navbar-menu fixed top-0 left-0 bottom-0 z-50 w-5/6 max-w-sm py-6 px-6 bg-background border-primary border-r overflow-y-auto transition-all duration-500 ease-in-out transform ${
+          isOpen
+            ? "translate-x-0 opacity-100"
+            : "-translate-x-full opacity-0 delay-150"
+        }`}
+      >
+        <nav className="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 w-full">
+          <div className="flex items-center mb-8">
+            <Link className="mr-auto text-3xl font-bold leading-none" href="/">
+              <Image
+                alt="Eros Elite Mobile logo"
+                className="dark:invert"
+                height={54}
+                priority
+                src="/EE_LOGO_SMALL.webp"
+                width={104}
+              />
+            </Link>
+            <button
+              className="text-foreground hover:text-primary"
+              onClick={() => setIsOpen(false)}
+              type="button"
+            >
+              <XIcon />
+            </button>
+          </div>
+          <ul>
+            {navigationLinks.map(({ label, href }) => (
+              <li key={label}>
+                <Link
+                  className="block text-base font-medium text-foreground py-2 px-4 rounded hover:text-primary"
+                  href={href}
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
