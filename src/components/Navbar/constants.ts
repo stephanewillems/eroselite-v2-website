685990324e01d156bbcd181c4e92c6bc0c48be77
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-09 22:50:53 +0100

    [EEMW-10] | add list of navigation links

---
+++ src/components/Navbar/constants.ts
@@ -0,0 +1,11 @@
+export const navigationLinks = [
+  { label: "Private", href: "/private" },
+  { label: "Escorte", href: "/escorte" },
+  { label: "Massages", href: "/massages" },
+  { label: "Fetisch", href: "/fetisch" },
+  { label: "Virtueel", href: "/virtueel" },
+  { label: "Adult Companies", href: "/adult-companies" },
+  { label: "Swinger", href: "/swinger" },
+  { label: "Vitrines", href: "/vitrines" },
+  { label: "Intimacy", href: "/intimacy" },
+];
