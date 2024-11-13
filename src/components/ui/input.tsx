e3667bb0d7302d815d55cf5318ba8fa8ea456a06
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-14 00:16:42 +0100

    add ability to use icon in input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,28 +8,63 @@ interface InputProps extends Omit<React.ComponentProps<"input">, "size"> {
   size?: Size;
 }
 
-const inputSizeMap: Record<Size, string> = {
-  sm: "h-7 file:text-sm md:text-sm",
-  md: "h-9 file:text-md md:text-md",
-  lg: "h-11 file:text-lg md:text-lg",
-};
-const defaultSize = inputSizeMap["md"];
+const getInputSizeClassname = (size: Size | undefined, hasIcon: boolean) => {
+  const inputSizeMap: Record<Size, string> = {
+    sm: cn("h-7 file:text-sm md:text-sm", {
+      "pl-8": hasIcon,
+    }),
+    md: cn("h-9 file:text-md md:text-md", {
+      "pl-10": hasIcon,
+    }),
+    lg: cn("h-11 file:text-lg md:text-lg", {
+      "pl-12": hasIcon,
+    }),
+  };
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, size, ...props }, ref) => {
-    const inputSizeClass = size ? inputSizeMap[size] : defaultSize;
+  return size ? inputSizeMap[size] : inputSizeMap["md"];
+};
+
+const getIconSizeClassname = (size: Size | undefined) => {
+  const iconSizeMap: Record<Size, string> = {
+    sm: "size-4",
+    md: "size-6",
+    lg: "size-8",
+  };
+
+  return size ? iconSizeMap[size] : iconSizeMap["md"];
+};
+
+interface CustomInputProps extends InputProps {
+  icon?: React.ComponentType<{
+    className?: string;
+  }>;
+}
+
+const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(
+  ({ icon: IconComponent, className, type, size, ...props }, ref) => {
+    const inputSizeClass = getInputSizeClassname(size, !!IconComponent);
+    const iconSizeClass = getIconSizeClassname(size);
 
     return (
-      <input
-        className={cn(
-          "flex w-full rounded-md border border-input bg-transparent px-3 py-1 text-foreground shadow-sm transition-colors file:border-0 file:bg-transparent file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          inputSizeClass,
-          className
-        )}
-        ref={ref}
-        type={type}
-        {...props}
-      />
+      <div className="relative h-full w-full max-w-sm items-center">
+        <input
+          className={cn(
+            "flex w-full rounded-md border border-input bg-transparent px-3 py-1 text-foreground shadow-sm transition-colors file:border-0 file:bg-transparent file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            inputSizeClass,
+            className
+          )}
+          ref={ref}
+          type={type}
+          {...props}
+        />
+        {IconComponent ? (
+          <span className="absolute start-0 inset-y-0 flex items-center justify-center px-2">
+            <IconComponent
+              className={cn(iconSizeClass, "text-muted-foreground")}
+            />
+          </span>
+        ) : null}
+      </div>
     );
   }
 );
