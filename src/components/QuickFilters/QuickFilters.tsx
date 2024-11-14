e9e96c8bb996d357565878e64fbbc371f2ae0410
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-14 21:46:42 +0100

    [EEMW-11] | add icons for age starting from and ending at

--- src/components/QuickFilters/QuickFilters.tsx
+++ src/components/QuickFilters/QuickFilters.tsx
@@ -1,5 +1,11 @@
 import { Input } from "@/components/ui/input";
-import { Circle, FilterIcon, Map, MessageCircleHeart } from "lucide-react";
+import {
+  Circle,
+  FilterIcon,
+  Map,
+  MessageCircleHeart,
+  UserRoundPlus,
+} from "lucide-react";
 import React from "react";
 import LocationSelect from "../filters/LocationSelect";
 import GenderSelect from "../filters/GenderSelect";
@@ -11,14 +17,24 @@ const QuickFilters = () => {
       <div className="text-base flex flex-1 overflow-hidden gap-three items-center lg:container">
         <div className="flex flex-1 items-center gap-one overflow-x-scroll hide-scrollbar lg:gap-two">
           <Button className="min-w-[35px]" size="icon" variant="primary">
-            <FilterIcon height={35} width={35}/>
+            <FilterIcon height={35} width={35} />
           </Button>
           <LocationSelect size="sm" />
           <Input icon={Map} placeholder="Distance" size="sm" />
           <Input icon={MessageCircleHeart} placeholder="Type" size="sm" />
           <GenderSelect icon={Circle} size="sm" />
-          <Input placeholder="Age starting from" size="sm" type="number" />
-          <Input placeholder="Age ending at" size="sm" type="number" />
+          <Input
+            icon={UserRoundPlus}
+            placeholder="Age starting from"
+            size="sm"
+            type="number"
+          />
+          <Input
+            icon={UserRoundPlus}
+            placeholder="Age ending at"
+            size="sm"
+            type="number"
+          />
         </div>
       </div>
     </div>
