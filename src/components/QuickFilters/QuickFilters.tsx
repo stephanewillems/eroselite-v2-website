78cf5589019fda7114c00298fca1e63c63fc1f34
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-14 21:41:03 +0100

    [EEMW-11] | add quick filter component

---
+++ src/components/QuickFilters/QuickFilters.tsx
@@ -0,0 +1,28 @@
+import { Input } from "@/components/ui/input";
+import { Circle, FilterIcon, Map, MessageCircleHeart } from "lucide-react";
+import React from "react";
+import LocationSelect from "../filters/LocationSelect";
+import GenderSelect from "../filters/GenderSelect";
+import { Button } from "../ui/button";
+
+const QuickFilters = () => {
+  return (
+    <div className="flex gap-4 border-y border-border-light p-one">
+      <div className="text-base flex flex-1 overflow-hidden gap-three items-center lg:container">
+        <div className="flex flex-1 items-center gap-one overflow-x-scroll hide-scrollbar lg:gap-two">
+          <Button className="min-w-[35px]" size="icon" variant="primary">
+            <FilterIcon height={35} width={35}/>
+          </Button>
+          <LocationSelect size="sm" />
+          <Input icon={Map} placeholder="Distance" size="sm" />
+          <Input icon={MessageCircleHeart} placeholder="Type" size="sm" />
+          <GenderSelect icon={Circle} size="sm" />
+          <Input placeholder="Age starting from" size="sm" type="number" />
+          <Input placeholder="Age ending at" size="sm" type="number" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QuickFilters;
