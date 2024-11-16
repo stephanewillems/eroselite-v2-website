72c7c456e85018ab225fcddda693921300fc5d27
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-16 21:23:28 +0100

    [EEMW-25] | show cards grid and banner components in homepage (temporary)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { CardsGrid } from "@/components/application/advertisements/cardsGrid";
 import { Banner } from "@/components/application/banner";
 
 const Home = () => {
   return (
     <div className="py-one">
       <Banner />
+      <CardsGrid />
+      <Banner />
+      <CardsGrid />
     </div>
   );
 };
