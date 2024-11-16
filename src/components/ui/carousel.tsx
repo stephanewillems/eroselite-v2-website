dd5601609286ca9a6b69f8c509ad67df42df6055
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-16 13:25:34 +0100

    [EEMW-24] | adapt the icons for carousel previous and next

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -3,7 +3,7 @@
 import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from "embla-carousel-react";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import * as React from "react";
 
 import { Button } from "@/components/ui/button";
@@ -216,7 +216,7 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       {...props}
     >
-      <ArrowLeft className="h-4 w-4" />
+      <ChevronLeft />
       <span className="sr-only">Previous slide</span>
     </Button>
   );
@@ -245,7 +245,7 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       {...props}
     >
-      <ArrowRight className="h-4 w-4" />
+      <ChevronRight />
       <span className="sr-only">Next slide</span>
     </Button>
   );
