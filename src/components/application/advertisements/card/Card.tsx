01e938df26085d4169812d1df32f061304cd92ca
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-16 21:09:23 +0100

    [EEMW-25] | create dummy card component

---
+++ src/components/application/advertisements/card/Card.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+
+const placeholderImageSrc = "/girl.svg";
+
+export const Card = () => {
+  return (
+    <div className="relative min-w-[250px] min-h-[250px] w-full h-full overflow-hidden aspect-square border border-eros-grey rounded-banner shadow-none transition-shadow duration-300 cursor-pointer hover:shadow-lg hover:shadow-primary">
+      <Image
+        alt="Card background"
+        className="object-cover"
+        fill
+        src={placeholderImageSrc}
+      />
+      <div className="absolute flex flex-col gap-2 bottom-0 left-0 w-full bg-background opacity-75 border-t border-eros-grey text-white p-one">
+        <div className="flex justify-between text-base">
+          <span>Ginger 10</span>
+          <span>Private</span>
+        </div>
+        <div className="flex justify-between text-base">
+          <span>29 Years</span>
+          <span>Antwerp</span>
+        </div>
+      </div>
+    </div>
+  );
+};
