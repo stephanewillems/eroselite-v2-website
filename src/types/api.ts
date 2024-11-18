22c99e05af7be56338d446b07a079148d83a2cf6
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-19 00:01:00 +0100

    [EEMW-17] | add types for an advertisement
    not as the api schema for now.

---
+++ src/types/api.ts
@@ -0,0 +1,17 @@
+// TODO: update this when the API is ready
+export type Advertisement = {
+  id: string;
+  title: string;
+  place: string;
+  cta: string;
+  description: string;
+  phone: string;
+  image1: {
+    src: string;
+    alt: string;
+  };
+  image2: {
+    src: string;
+    alt: string;
+  };
+};
