cd3057d840fa590609861abd14f7802a3cc64586
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-19 22:33:04 +0100

    [EEMW-14] | add new hook for fetching paginated data

---
+++ src/hooks/usePaginatedData.tsx
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+
+export type Fetcher<T> = (path: string) => Promise<T>;
+
+function usePaginatedData<T>(
+  path: string,
+  fetcher: Fetcher<T[]>,
+  { pageNumber = 1, pageSize = 10 }: { pageNumber: number; pageSize: number }
+) {
+  const key = `${path}&page=${pageNumber}&size=${pageSize}`;
+  const { data, error, isValidating } = useSWR<T[]>(key, () => fetcher(key));
+
+  const totalItems = data?.length || 0; // this should be coming from BE as a meta field
+  const totalPages = Math.ceil(totalItems / pageSize);
+
+  return {
+    data: data || [],
+    isLoading: isValidating,
+    error,
+    totalItems,
+    totalPages,
+  };
+}
+
+export default usePaginatedData;
