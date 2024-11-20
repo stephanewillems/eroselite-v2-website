4961331618657d990c2336c63533eb8f4c1facf4
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-20 23:31:59 +0100

    [EEMW-14] | use vitrines page as example

---
+++ src/app/vitrines/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { PaginatedContent } from "@/components/templates/PaginatedPageContent";
+
+const Vitrines = () => {
+  return (
+    <div>
+      <h1>Vitrines</h1>
+      <PaginatedContent />
+    </div>
+  );
+};
+
+export default Vitrines;
