0ab8bf8390ed9d810962ae3cef1e111d62410e55
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-20 23:02:56 +0100

    [EEMW-14] | make useInfiniteScroll reusable by path

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -1,15 +1,27 @@
 import useSWRInfinite from "swr/infinite";
 
-import { advertisementsFetcher, AdvertisementType } from "@/api/fetcher";
-import { Advertisement } from "@/types/api";
+import { Fetcher } from "@/api/fetcher";
 
-const useInfiniteScroll = (type: AdvertisementType) => {
+const defaultPageConfig = {
+  pageSize: 16,
+  initialPageNumber: 1,
+};
+
+// TODO: remove fetcher from the future, we should be able to use only the key to fetch the data. The fetcher will be generic for our whole API
+function useInfiniteScroll<T extends unknown[]>(
+  path: string,
+  fetcher: Fetcher<T>,
+  pageConfig?: { pageSize: number; initialPageNumber: number }
+) {
+  const { pageSize, initialPageNumber } = pageConfig || defaultPageConfig;
+
   const key = (index: number) =>
-    `/api/advertisements?type=${type}&page=${index + 1}`;
+    `${path}&page=${index + initialPageNumber}&size=${pageSize}`;
 
-  const { data, isValidating, isLoading, setSize } = useSWRInfinite<
-    Advertisement[]
-  >(key, advertisementsFetcher);
+  const { data, isValidating, isLoading, setSize } = useSWRInfinite<T>(
+    key,
+    fetcher
+  );
 
   const items = data ? data.flat() : [];
   const hasReachedEnd = data?.[data.length - 1]?.length === 0;
@@ -24,6 +36,6 @@ const useInfiniteScroll = (type: AdvertisementType) => {
     hasReachedEnd,
     loadMore,
   };
-};
+}
 
 export default useInfiniteScroll;
