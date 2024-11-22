3c55fce742b638a3dc16377d68438cca191863c8
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-22 19:32:18 +0100

    [EEMW-14] | remove previous and next wordings, use only icons

--- src/components/ui/pagination/components.tsx
+++ src/components/ui/pagination/components.tsx
@@ -71,8 +71,7 @@ const PaginationPrevious = ({
     className={cn("gap-1 pl-2.5", className)}
     {...props}
   >
-    <ChevronLeft className="h-4 w-4" />
-    <span>Previous</span>
+    <ChevronLeft className="h-12 w-12" />
   </PaginationLink>
 );
 PaginationPrevious.displayName = "PaginationPrevious";
@@ -86,8 +85,7 @@ const PaginationNext = ({
     className={cn("gap-1 pr-2.5", className)}
     {...props}
   >
-    <span>Next</span>
-    <ChevronRight className="h-4 w-4" />
+    <ChevronRight className="h-12 w-12" />
   </PaginationLink>
 );
 PaginationNext.displayName = "PaginationNext";
