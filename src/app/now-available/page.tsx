aaa1104221a9a9ecc3e7bc531fb7d3d752621ab5
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-22 20:58:07 +0100

    [EEMW-14] | use Page grid type for now available page

---
+++ src/app/now-available/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { PaginatedPage } from "@/components/templates/PaginatedPage";
+
+const NowAvailablePage = () => {
+  return (
+    <div>
+      <PaginatedPage layoutType="grid" />
+    </div>
+  );
+};
+
+export default NowAvailablePage;
