a84a4e30e244ff8f83fc3a1829b173852cdf02a2
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-22 20:55:54 +0100

    [EEMW-14] | Use the content generator component in PaginatedPage

--- src/components/templates/PaginatedPage/PaginatedPage.tsx
+++ src/components/templates/PaginatedPage/PaginatedPage.tsx
@@ -8,7 +8,7 @@ import { Banner } from "@/components/application/banner";
 import { Pagination } from "@/components/ui/pagination";
 import usePaginatedData from "@/hooks/usePaginatedData";
 
-export const PaginatedPage = () => {
+import { ContentGenerator } from "./ContentGenerator";
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get("page")) || 1;
 
@@ -35,37 +35,16 @@ export const PaginatedPage = () => {
   const isLoading = bannerItemsAreLoading || categoryItemsAreLoading;
   const totalNumberOfPages = Math.max(bannerTotalPages, categoryTotalPages);
 
-  // Banner items
-  const bannerPartSize = Math.ceil(bannerItems.length / 3);
-  const firstBannerItems = bannerItems.slice(0, bannerPartSize);
-  const secondBannerItems = bannerItems.slice(
-    bannerPartSize,
-    bannerPartSize * 2
-  );
-  const thirdBannerItems = bannerItems.slice(bannerPartSize * 2);
-
-  // Category items
-  const cardGridPartSize = Math.ceil(categoryItems.length / 2);
-  const firstCardGridItems = categoryItems.slice(0, cardGridPartSize);
-  const secondCardGridItems = categoryItems.slice(
-    cardGridPartSize,
-    cardGridPartSize * 2
-  );
-
   return (
     <div className="py-two flex flex-col gap-four">
       {isLoading ? (
         <p>Loading...</p>
       ) : (
         <>
-          {/* // TODO: lets find a way of making this more smart, generating the banners and cards dynamically based on the size we want. */}
-          <div className="flex flex-col gap-two">
-            <Banner items={firstBannerItems} />
-            <CardsGrid items={firstCardGridItems} />
-            <Banner items={secondBannerItems} />
-            <CardsGrid items={secondCardGridItems} />
-            <Banner items={thirdBannerItems} />
-          </div>
+          <ContentGenerator
+            bannerItems={bannerItems}
+            categoryItems={categoryItems}
+          />
           <Pagination
             currentPage={currentPage}
             totalPages={totalNumberOfPages}
