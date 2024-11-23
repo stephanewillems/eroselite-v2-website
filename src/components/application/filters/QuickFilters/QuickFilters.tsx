89a8cc64471b19f6057bcd2973d6f4f3904eb04d
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-23 18:46:41 +0100

    [EEMW-16] | show filter popover in the quick filters list

--- src/components/application/filters/QuickFilters/QuickFilters.tsx
+++ src/components/application/filters/QuickFilters/QuickFilters.tsx
@@ -1,6 +1,5 @@
 import {
   Circle,
-  FilterIcon,
   Map,
   MessageCircleHeart,
   UserRoundPlus,
@@ -8,17 +7,16 @@ import {
 import React from "react";
 
 import { LocationSelect, GenderSelect } from "@/components/application/filters";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+
+import { FilterPopover } from "../FilterPopover";
 
 export const QuickFilters = () => {
   return (
     <div className="bg-background flex gap-4 border-b border-border-light p-one">
       <div className="text-base flex flex-1 overflow-hidden gap-three items-center lg:container px-one">
         <div className="flex flex-1 items-center gap-one overflow-x-scroll hide-scrollbar lg:gap-two">
-          <Button className="min-w-[35px]" size="icon" variant="primary">
-            <FilterIcon height={35} width={35} />
-          </Button>
+          <FilterPopover />
           <LocationSelect size="sm" />
           <Input icon={Map} placeholder="Distance" size="sm" />
           <Input icon={MessageCircleHeart} placeholder="Type" size="sm" />
