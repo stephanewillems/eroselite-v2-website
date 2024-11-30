04fffe62acf4f892fdd705b401fff3bf05e2f484
Enis Krasniqi <enis.kraasniqi@mestadvertising.be>
2024-11-30 13:38:00 +0100

    [EEMW-9] | built profile page using the components

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,5 +1,13 @@
+import { ImageGallery } from "@/components/application/profile/ImageGallery";
+import { InfoWidget } from "@/components/application/profile/InfoWidget";
+
 const UserProfilePage = () => {
-  return <div />;
+  return (
+    <div className="flex flex-col w-full py-one gap-three basis-3/5 lg:flex-row">
+      <ImageGallery />
+      <InfoWidget />
+    </div>
+  );
 };
 
 export default UserProfilePage;
